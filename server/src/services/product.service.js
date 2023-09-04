883daeb9f626e631f23ca3ee13dccd3d62f8126f
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-09-04 22:54:25 +0300

    :seedling: add(product): add get all  products to service

--- server/src/services/product.service.js
+++ server/src/services/product.service.js
@@ -4,6 +4,12 @@ const createProduct = async (productBody) => {
   return Product.create(productBody);
 };
 
+const getAllProducts = async () => {
+  const products = await Product.find();
+  return products;
+};
+
 module.exports = {
   createProduct,
+  getAllProducts,
 };
