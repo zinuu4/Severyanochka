67657b338d860af830534cbe2dc018a8d5fda914
zinuu4 <unvm7721@gmail.com>
2023-09-12 19:00:56 +0300

    feat: footer basic styles and bg

---
+++ client/src/widgets/footer/ui/index.tsx
@@ -0,0 +1,12 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export const Footer = () => (
+  <footer className={styles.footer}>
+    <section className={clsx('container', styles.container)}>
+      <div />
+    </section>
+  </footer>
+);
